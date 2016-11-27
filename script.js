b4446b48687862c72fd1313def9967c46ee6420d
Aurelien Roux <aureroux13@me.com>
2016-11-27 16:18:08 -0500

    put options in getJSON function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(function(){
 
 	var feed = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var options = {
+		"format" : "json"
+	};
 
-
-	$.getJSON(feed, function(result){
+	$.getJSON(feed, options, function(result){
 		alert("test");
 	});
 });
