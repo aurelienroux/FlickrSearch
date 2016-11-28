1ce6796be756554832a789385f901fe3b6277632
Aurelien Roux <aureroux13@me.com>
2016-11-27 19:03:00 -0500

    create ajax feed with values and callback function

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ $(function(){
 		//var with input field value
 		var tag = $("input").val();
 
+		//ajax part
+		var feed = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+		var options = {
+			format : "json"
+		}
+
+		function display(data){
+			console.log(data.items[0].link);
+		};
+
+		$.getJSON(feed, options, display);
+
 	})
 
 
